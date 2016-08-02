8d8c5dbb1f3d32cb8ae6d7e02295e4e137f70c0a
Richard Bunn <bunnrf@gmail.com>
2016-08-01 19:13:56 -0700

    context menu, removed jquery

--- background.js
+++ background.js
@@ -1,5 +1,20 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.sendMessage(tab.id, { message: "clickrequest" }, function(response) {
-    // alert(response.message);
+  chrome.tabs.sendMessage(tab.id, { message: "clickRequest" }, function(response) {
+    console.log(response);
   });
 });
+
+const contexts = ["page","selection","link","editable","image","video", "audio"];
+for (let i = 0; i < contexts.length; i++) {
+  const context = contexts[i];
+  const title = "Test '" + context + "' menu item";
+  const id = chrome.contextMenus.create({"title": title, "contexts":[context],
+                                       "onclick": contextMenuCallback});
+  console.log("'" + context + "' item:" + id);
+}
+
+function contextMenuCallback(info, tab) {
+  chrome.tabs.sendMessage(tab.id, { message: "clickFromContext" }, function(response) {
+    console.log(response);
+  });
+}
--- inject.js
+++ inject.js
@@ -1,34 +1,55 @@
+// for Context Menus
+let contextEl = null;
+document.addEventListener("contextmenu", function(event) {
+  contextEl = event.target
+});
+
+// click listeners not triggered when target is <input>, so we use mouseup
 function getElementFromClick(callback) {
   function handleClick(event) {
     window.removeEventListener("mouseup", handleClick, false);
     callback(event.target);
   }
-  // click listeners not triggered when target is input
   window.addEventListener("mouseup", handleClick, false);
 }
 
-function clickElements(elementClickedByUser, callback, errorCallback) {
-  const elements = Array.from($("." + elementClickedByUser.className));
-  elements.splice(elements.indexOf(elementClickedByUser), 1);
-
-  if (elements.length > 0) {
-    $(elements).click();
-    callback(elements.length);
-  } else {
+function clickElements(elements, callback, errorCallback) {
+  if (elements.length < 1) {
     errorCallback("Error finding elements");
+    return;
   }
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].click();
+  }
+  callback(elements.length);
+}
+
+function getMatchingElements(element) {
+  return document.getElementsByClassName(element.className);
 }
 
 chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
-    if (request.message === "clickrequest") {
-      getElementFromClick((elementClickedByUser) => {
-        clickElements(elementClickedByUser, (count) => {
-          sendResponse({ message: count + " elements clicked" });
-        }, (errorMessage) => {
-          sendResponse({ message: "Autoclicking failed: " + errorMessage });
-        } );
-      });
-    }
-    // return true to require asynchronous call of sendResponse
-    return true;
+  if (request.message === "clickRequest") {
+    getElementFromClick((elementClickedByUser) => {
+      const matchingElements = Array.from(getMatchingElements(elementClickedByUser));
+      matchingElements.splice(matchingElements.indexOf(elementClickedByUser), 1);
+      clickElements(matchingElements, (elementClicked, count) => {
+        sendResponse({ message: "success", element: elementClicked, count: count });
+      }, (errorMessage) => {
+        sendResponse({ message: "Autoclicking failed: " + errorMessage });
+      } );
+    });
+  }
+
+  if(request.message === "clickFromContext") {
+    const matchingElements = getMatchingElements(contextEl);
+    clickElements(matchingElements, (count) => {
+      console.log(contextEl);
+      sendResponse( { contextEl: contextEl } );
+    }, (errorMessage) => {
+      sendResponse({ message: "Autoclicking failed: " + errorMessage });
+    })
+  }
+  // return true to require asynchronous call of sendResponse
+  return true;
 });
